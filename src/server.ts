90e3128d7e1d5cbecccb8244d45f8e20963c48fa
Leticia Sant'anna <leticia.santanna@escolaconquer.com.br>
2024-03-25 13:01:41 -0300

    created initial saves in databases

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,25 @@
 import fastify from "fastify"
+import {z} from 'zod'
+import { sql } from "./lib/postgres"
 
 const app = fastify()
 
-app.get('/teste', () => {
- return 'Oie'
+app.post('/links', async (request, reply) => {
+ const schema = z.object({
+  code: z.string().min(3),
+  url: z.string().url()
+ })
+ const { code, url} = schema.parse(request.body) 
+
+ const result = await sql/*sql*/`
+ INSERT INTO short_link (code, original_url)
+ VALUES(${code}, ${url})
+ RETURNING id
+ `
+
+ const link = result[0]
+
+ return reply.status(201).send({shortLinkId: link.id})
 })
 
 app.listen({
