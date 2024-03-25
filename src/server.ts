08a817130c798668b79075ee6a95f4893c16f07c
Leticia Sant'anna <leticia.santanna@escolaconquer.com.br>
2024-03-25 11:16:16 -0300

    install fastify

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log('Teste')
+import fastify from "fastify"
+
+const app = fastify()
+
+app.get('/teste', () => {
+ return 'Oie'
+})
+
+app.listen({
+ port: 3333,
+}).then(() => console.log('server is running'))
